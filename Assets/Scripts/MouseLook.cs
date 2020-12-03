92ddff991a7e8237d7d661d20781decc070d7cfb
Eddy <eddy.abada.ea@gmail.com>
2020-12-03 17:58:42 +0100

    basic player movements

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform PlayerBody;
+    float x_rotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;    
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        x_rotation -= mouseY;
+        //clamp to make sure the camera will not rotate over the PlayerBody
+        x_rotation = Mathf.Clamp(x_rotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(x_rotation, 0f, 0f);
+        PlayerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+
+    Vector3 velocity;
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //target the direction where the Player wants to move (according to its transform)
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        //simulate gravity
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
