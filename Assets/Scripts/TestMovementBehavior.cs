e802152a076bf76c56b76ddd67dad0f6fd42be19
Mikaël <mika.fr99@hotmail.com>
2020-12-03 17:20:40 +0100

    NavMesh Setup
    - NavMesh Creation
    - NavMesh Agent Creation and basic movement
    - Basic Material creation

---
+++ Assets/Scripts/TestMovementBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TestMovementBehavior : MonoBehaviour
+{
+    public GameObject goal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            NavMeshAgent agent = GetComponent<NavMeshAgent>();
+            agent.destination = goal.transform.position;
+        }
+    }
+}
