41fc3490cf3eefc8fed2d3f90c46c596bd859499
Eddy <eddy.abada.ea@gmail.com>
2020-12-04 00:25:13 +0100

    player controller update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,10 +8,25 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = 12f;
     public float gravity = -9.81f;
+    public float jump_height = 3f;
+
+    //allows to check if Player is grounded
+    public Transform GroundCheck;
+    public float ground_distance = 0.4f;
+    public LayerMask GroundMask;
+
+    bool is_grounded;
 
     Vector3 velocity;
     void Update()
     {
+        is_grounded = Physics.CheckSphere(GroundCheck.position, ground_distance, GroundMask);
+
+        if(is_grounded && velocity.y < 0f)
+        {
+            //force Player to the ground
+            velocity.y = -2f;
+        }
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
@@ -19,6 +34,11 @@ public class PlayerMovement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && is_grounded)
+        {
+            velocity.y = Mathf.Sqrt(jump_height * -2f * gravity);
+        }
 
         //simulate gravity
         velocity.y += gravity * Time.deltaTime;
