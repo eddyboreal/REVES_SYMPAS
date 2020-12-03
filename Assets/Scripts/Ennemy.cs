02a25383da1101cc392387e75fa71227b3bd9838
Eddy <eddy.abada.ea@gmail.com>
2020-12-04 00:53:42 +0100

    gun raycast

---
+++ Assets/Scripts/Ennemy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Ennemy : MonoBehaviour
+{
+    public float health = 50f;
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (health == 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera FPSCam;
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    //use raycast to shoot
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(FPSCam.transform.position, FPSCam.transform.forward, out hit, range))
+        {
+            Ennemy ennemy = hit.transform.GetComponent<Ennemy>();
+            if (ennemy)
+            {
+                ennemy.TakeDamage(damage);
+            }
+        }
+    }
+}
