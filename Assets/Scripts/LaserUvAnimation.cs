500a09ac3a03d3cefed968918d4c99f96cf1370a
Eddy <eddy.abada.ea@gmail.com>
2020-12-05 18:59:36 +0100

    laser shader try (not concluding atp)

---
+++ Assets/Scripts/LaserUvAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserUvAnimation : MonoBehaviour
+{
+    public LineRenderer Laser;
+    public float i_speed;
+
+    private Material m_material;
+    private float m_time;
+
+    void Start()
+    {
+        m_material = Laser.material;
+
+        m_time = 0.0f;
+    }
+
+    void Update()
+    {
+        m_time += Time.deltaTime * i_speed;
+
+        m_material.SetFloat("_Offset", Mathf.Repeat(m_time, 1.0f));
+    }
+}
